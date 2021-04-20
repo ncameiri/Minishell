2352a19a08ef813d36fdb3aa091a0972306b3b75
Olbrien <77759096+Olbrien@users.noreply.github.com>
2021-04-20 16:10:37 +0100

    Add files via upload

--- tisantos/shell.c
+++ tisantos/shell.c
@@ -6,35 +6,42 @@
 /*   By: tisantos <tisantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 23:57:53 by tisantos          #+#    #+#             */
-/*   Updated: 2021/04/19 00:48:55 by tisantos         ###   ########.fr       */
+/*   Updated: 2021/04/20 16:08:14 by tisantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-char *lsh_read_line(void)
+char **shell_split_arguments(char *line)
 {
-	char *line = NULL;
-	ssize_t bufsize = 0; // have getline allocate a buffer for us
+	char **args;
 
-	if (getline(&line, &bufsize, stdin) == -1)
-	{
-		if (feof(stdin))
-		{
-			exit(EXIT_SUCCESS);  // We recieved an EOF
-		}
-		else
-		{
-			perror("readline");
-			exit(EXIT_FAILURE);
-		}
-	}
+	args = NULL;
+	args = ft_split_chars(line, SHELL_DELIMITERS);
 
-	return line;
+	return (args);
 }
 
+char *shell_prompt(void)
+{
+	char *line;
+	char buf[4096 + 1];
+	char *path;
 
-void lsh_loop(void)
+	line = NULL;
+	path = getcwd(buf, 4096);
+
+	write(1, "\x1b[33m", 6);
+	write(1, path, ft_strlen(path));
+	write(1, "\x1b[0m", 5);
+	write(1, "$ ", 3);
+
+	get_next_line(0, &line);
+
+	return (line);
+}
+
+void shell_loop(char **env)
 {
 	char *line;
 	char **args;
@@ -42,31 +49,27 @@ void lsh_loop(void)
 
 	do
 	{
-		printf("> ");
-		line = lsh_read_line();
-		//args = lsh_split_line(line);
-		//status = lsh_execute(args);
+		line = shell_prompt();
+		args = shell_split_arguments(line);
+		//status = shell_execute(args);
+
+		//int execvp(const char *__file, char *const *__argv)
+		//int execve(const char *__path, char **__argv, char **__envp)
+
+
+		execve(args[0], args, env);
 
 		free(line);
-		//free(args);
+		free_array(args);
 	} while (status);
 }
 
 
 
-int main (int argc, char **argv)
+int main (int argc, char **argv, char **env)
 {
 
-	// Load config files, if any.
-
-	// Run command loop.
-	//lsh_loop();
-
-	// Perform any shutdown/cleanup.
-	char *line = NULL;
-	ssize_t bufsize = 0;
-	//getline(&line, &bufsize, stdin);
-	get_next_line(0, &line);
+	shell_loop(env);
 
 	return (EXIT_SUCCESS);
 }
--- tisantos/shell.h
+++ tisantos/shell.h
@@ -6,7 +6,7 @@
 /*   By: tisantos <tisantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 23:59:14 by tisantos          #+#    #+#             */
-/*   Updated: 2021/04/19 00:04:01 by tisantos         ###   ########.fr       */
+/*   Updated: 2021/04/20 14:11:48 by tisantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/wait.h>
+# include <string.h>
+
 
 # define LSH_RL_BUFSIZE 1024
 # define LSH_TOK_BUFSIZE 64
-# define LSH_TOK_DELIM " \t\r\n\a"
+# define SHELL_DELIMITERS " \t\r\n\a"
 
 #endif
